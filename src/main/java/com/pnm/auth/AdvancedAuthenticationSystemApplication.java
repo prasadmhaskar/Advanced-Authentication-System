7be2aefe3be0683c9f721373715790eb157b849b
Prasad Mhaskar <prasadmhaskar33@gmail.com>
2025-11-23 19:39:36 +0530

    Initial commit - Project 1 setup with Spring Boot 3 and JDK 17

---
+++ src/main/java/com/pnm/auth/AdvancedAuthenticationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pnm.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedAuthenticationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedAuthenticationSystemApplication.class, args);
+	}
+
+}
